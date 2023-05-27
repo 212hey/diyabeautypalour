2ec2077d2f4704223db91ccc1bf7d0d1a8c60fbb
212hey <134798982+212hey@users.noreply.github.com>
2023-05-27 10:05:15 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//varibles
+let hs=document.querySelector("#bthhs");
+let p1=document.querySelector(".p1");
+let hide = document.querySelector('#hide');
+//funcions
+/*function displayHairStyle(){
+    if (p1.style.display="none") {
+        p1.style.display="block";
+    }
+     else if(p1.style.display="block") {
+        dhsb()
+    }
+}
+function dhsb(){
+    p1.style.width='50%'
+}
+hide.onclick=p1.style.display='none';
+hs.onclick=p1.style.display='block';
+p1.style.backgroundColor='red=';
+alert("hi")*/
